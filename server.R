343e76201c37e7a343741de9cf3983d34895d259
SamuelYJKim <syjkim75@uw.edu>
2019-11-15 13:53:56 -0800

    test title

--- server.R
+++ server.R
@@ -1,23 +1,6 @@
 library(shiny)
-shinyServer(fluidPage(
-  
-  # Application title
-  titlePanel("Hello Shiny!"),
-  
-  sidebarLayout(
+shinyServer(
+  function(input, output) {
     
-    # Sidebar with a slider input
-    sidebarPanel(
-      sliderInput("obs",
-                  "Number of observations:",
-                  min = 0,
-                  max = 1000,
-                  value = 500)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
-  )
-))
+  }
+)
